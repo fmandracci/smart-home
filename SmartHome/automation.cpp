47eb45fa8201dce615433aeb69ff1e0cb1dda259
Francesco <francesco.mandracci@gmail.com>
2022-12-02 23:01:26 +0100

    v1.2_04 introducing control type 16 (EP+Tn+BA) release candidate

--- SmartHome/automation.cpp
+++ SmartHome/automation.cpp
@@ -57,7 +57,7 @@ void setup(void)
         fputc('*', stderr);
         sleep(1);
     }
-    doWrite_PLC_HMI_Version(1203);
+    doWrite_PLC_HMI_Version(1204);
 
     // --------- system ---------------------------------------------
     doWrite_PLC_timeWin(300); // graph: 5'
