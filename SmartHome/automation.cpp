704fc46c2b6b5a9ded183cb919a6b3fc5daab09e
Francesco Mandracci <francesco.mandracci@gmail.com>
2025-05-05 19:06:23 +0200

    v2.0_07: new ControlType_9: TPX* + MPNE_E: EP(4) + BA(8)

--- SmartHome/automation.cpp
+++ SmartHome/automation.cpp
@@ -77,7 +77,7 @@ void setup(void)
         fputc('*', stderr);
         sleep(1);
     }
-    doWrite_PLC_HMI_Version(2006);
+    doWrite_PLC_HMI_Version(2007);
 
     // --------- system ---------------------------------------------
     doWrite_PLC_timeWin(300); // graph: 5'
